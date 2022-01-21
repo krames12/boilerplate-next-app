5b220efec90aded054767353ef1fcf5987a0e2d6
Nick Kramer <krames12@gmail.com>
2022-01-21 06:45:59 -0500

    adjusts nav padding and hover color

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -29,7 +29,7 @@ const Navigation = () => {
       <ul className={styles["nav-menu-listing"]}>
         <NavigationItem
           isExpanded={isExpanded}
-          content="Dashboard"
+          content="Home"
           Icon={BiHomeAlt}
         />
         <NavigationItem
